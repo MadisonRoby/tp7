e1e8b18d6715d8e49085419335502f68021802ed
Madison Roby <madison.roby@wsu.edu>
2021-03-24 07:27:01 +0000

    fixed image issue

--- scripts.js
+++ scripts.js
@@ -28,39 +28,47 @@ function loadFileInto(fromFile, whereTo) {
 }
   //object constructor
 
-function Recipe(recipeName, contributorName, ingredientFileName, equipmentFileName, directionsFileName) {
+function Recipe(recipeName, contributorName, imageURL, ingredientFileName, equipmentFileName, directionsFileName) {
   this.name = recipeName;
-  this.contributor = contributorName
+  this.contributor = contributorName;
+  this.imgsrc = imageURL;
   this.ingFile = ingredientFileName;
   this.equipFile = equipmentFileName;
   this.dirFile = directionsFileName;
   
+  
   this.displayRecipe = function() {
   document.querySelector("#title").innerHTML = this.name;
   document.querySelector("#contributor").innerHTML = "Contributed by: " + this.contributor;
+  document.querySelector("#header").style.backgroundImage = "url(" + this.imgsrc + ")";
   loadFileInto(this.ingFile, "ingredients");
   loadFileInto(this.equipFile, "equipment");
   loadFileInto(this.dirFile, "directions");
   
+  
   }
 }
 
-Lasagna = new Recipe("Lasagna", 
+Lasagna = new Recipe("World's Best Lasagna", 
                      "Madison",
+                     "https://cdn.pixabay.com/photo/2017/02/15/15/17/meal-2069021_1280.jpg",
                      "ingredients.html",
                      "equipment.html",
-                     "directions.html"
+                     "directions.html",
+                     
                     );
 
-Chicken = new Recipe ( "Chicken",
+Chicken = new Recipe ( "Lemon Chicken Piccata",
                        "Sophie",
+                       "https://sdutton355.com/tp4/chicken.jpg",
                        "chickeningredients.html",
                        "chickenequipment.html",
                        "chickendirections.html"
                      );
 
-Pretzel = new Recipe ( "Pretzel",
+Pretzel = new Recipe ( "Soft Pretzels",
                        "Kristine",
+                       "https://kzorn355.com/tp4/images/pretzel.jpg",
                        "pretzelingredients.html",
                        "pretzelequipment.html",
                        "pretzeldirections.html"
